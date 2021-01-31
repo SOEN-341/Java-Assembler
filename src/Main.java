efd4960fa0021ff85dbd6e6916f74cd0cdc27dea
Kerolos Wahba <kerodwar@ymail.com>
2021-01-31 14:31:06 -0500

    test for committing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java Assembler");
+        System.out.println("Test for committing an update");
     }
 }
