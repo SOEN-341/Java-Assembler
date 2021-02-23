0fa89b92f9acf1bdabe776341e9ee6295d401739
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2021-02-22 19:18:37 -0500

    Completed SymbolTable

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -5,29 +5,29 @@ public class SymbolTable implements ISymbolTable{
     public SymbolTable(){
         this.Table = new Hashtable();
         Table.put("hlt", new Mnemonic("hlt", 0x00)); ;
-        Table.put("pop", 0x01); new Mnemonic("hlt", 0x00);
-        Table.put("exit", 0x03); new Mnemonic("hlt", 0x00);
-        Table.put("ret", 0x04); new Mnemonic("hlt", 0x00);
-        Table.put("not", 0x0C); new Mnemonic("hlt", 0x00);
-        Table.put("and", 0x0D); new Mnemonic("hlt", 0x00);
-        Table.put("or", 0x0E); new Mnemonic("hlt", 0x00);
-        Table.put("xor", 0x0F);new Mnemonic("hlt", 0x00);
-        Table.put("neg", 0x10);new Mnemonic("hlt", 0x00);
-        Table.put("inc", 0x11);
-        Table.put("dec", 0x12);
-        Table.put("add", 0x13);
-        Table.put("sub", 0x14);
-        Table.put("mul", 0x15);
-        Table.put("div", 0x16);
-        Table.put("rem", 0x17);
-        Table.put("shl", 0x18);
-        Table.put("shr", 0x19);
-        Table.put("teq", 0x1A);
-        Table.put("tne", 0x1B);
-        Table.put("tlt", 0x1C);
-        Table.put("tgt", 0x1D);
-        Table.put("tle", 0x1E);
-        Table.put("tge", 0x1F);
+        Table.put("pop", new Mnemonic("pop", 0x01));
+        Table.put("exit", new Mnemonic("exit", 0x03));
+        Table.put("ret", new Mnemonic("ret", 0x04));
+        Table.put("not", new Mnemonic("not", 0x0C));
+        Table.put("and", new Mnemonic("and", 0x0D));
+        Table.put("or", new Mnemonic("or", 0x0E));
+        Table.put("xor", new Mnemonic("xor", 0x0F));
+        Table.put("neg", new Mnemonic("neg", 0x10));
+        Table.put("inc", new Mnemonic("inc", 0x11));
+        Table.put("dec", new Mnemonic("dec", 0x12));
+        Table.put("add", new Mnemonic("add", 0x13));
+        Table.put("sub", new Mnemonic("sub", 0x14));
+        Table.put("mul", new Mnemonic("mul", 0x15));
+        Table.put("div", new Mnemonic("div", 0x16));
+        Table.put("rem", new Mnemonic("rem", 0x17));
+        Table.put("shl", new Mnemonic("shl", 0x18));
+        Table.put("shr", new Mnemonic("shr", 0x19));
+        Table.put("teq", new Mnemonic("teq", 0x1A));
+        Table.put("tne", new Mnemonic("tne", 0x1B));
+        Table.put("tlt", new Mnemonic("tlt", 0x1C));
+        Table.put("tgt", new Mnemonic("tgt", 0x1D));
+        Table.put("tle", new Mnemonic("tle", 0x1E));
+        Table.put("tge", new Mnemonic("tge", 0x1F));
 
     }
 
