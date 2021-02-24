b17400cf805690fa6b48ba577b9cb05e7c166f3a
Kerolos Wahba <kerodwar@ymail.com>
2021-02-23 22:32:55 -0500

    Change Directory of Test Files

--- copiedTestInherentMnemonics.asm
+++ copiedTestInherentMnemonics.asm
@@ -24,29 +24,3 @@
  tle 
  tge 
  halt
- halt
- pop
- dup
- exit
- ret
- not 
- and 
- or  
- xor 
- neg 
- inc 
- dec 
- add 
- sub 
- mul 
- div 
- rem 
- shl 
- shr
- teq 
- tne 
- tlt 
- tgt 
- tle 
- tge 
- halt
