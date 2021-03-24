70738fe072065e046d8c5d9b07f16ef6381799a1
SarahSalib <sarah_salib@yahoo.com>
2021-03-23 21:33:08 -0400

    changed test (parser,line statement, IR) changed parser interface

--- test/TestInstruction.java
+++ test/TestInstruction.java
@@ -1,11 +1,11 @@
 public class TestInstruction {
     public static void main(String[] args) {
-        Instruction Instruct1 = new Instruction("halt", "i3");
-        Instruction Instruct2 = new Instruction("stv", "u3");
+        Instruction Instruct1 = new Instruction("enter.i3", "-1");
+        Instruction Instruct2 = new Instruction("stv", "");
         Instruction Instruct3 = new Instruction("dup","");
 
         System.out.println("Test Instruction");
-        System.out.println("Instruct1[halt,i3]\tInstruct2[stv,u3]\tInstruct3[dup,]");
+        System.out.println("Instruct1[enter.i3,-1]\tInstruct2[stv,]\tInstruct3[dup,]");
 
         System.out.print("Instruct1["+ Instruct1.getMnemonic()+","+ Instruct1.getOperand()+"]");
         System.out.print("\tInstruct2["+ Instruct2.getMnemonic()+","+ Instruct2.getOperand()+"]");
--- test/TestLineStatement.java
+++ test/TestLineStatement.java
@@ -1,12 +1,12 @@
 public class TestLineStatement {
     public static void main(String[] args) {
-        LineStatement Line1 = new LineStatement("Done", "halt", "i3", "Nothing");
-        LineStatement Line2 = new LineStatement("loop", "xor", "i3", "");
-        LineStatement Line3 = new LineStatement("continue", "rem", "", "");
+        LineStatement Line1 = new LineStatement("Done", "enter.u5", "28", "Nothing");
+        LineStatement Line2 = new LineStatement("loop", "xor", "", "");
+        LineStatement Line3 = new LineStatement("continue", "addv.i3", "-1", "");
         System.out.println("Test LineStatement");
-        System.out.println("LineStatement{label=Done, Instruction=[Mnemonic=halt, Operand=i3], Comments=Nothing}"
-        +"\tLineStatement{label=loop, Instruction=[Mnemonic=xor, Operand=i3], Comments=}"
-        +"\tLineStatement{label=continue, Instruction=[Mnemonic=rem, Operand=], Comments=}");
+        System.out.println("LineStatement{label=Done, Instruction=[Mnemonic=enter.u5, Operand=28], Comments=Nothing}"
+        +"\tLineStatement{label=loop, Instruction=[Mnemonic=xor, Operand=], Comments=}"
+        +"\tLineStatement{label=continue, Instruction=[Mnemonic=addv.i3, Operand=-1], Comments=}");
 
 
         System.out.print(Line1);
