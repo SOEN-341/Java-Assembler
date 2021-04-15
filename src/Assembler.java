240bd7731fc80514946ba28a370292ec268f6150
Andre Ibrahim <psnmeee@gmail.com>
2021-04-15 15:30:15 -0400

    validating that file exists and added functionaltity to the assembler

--- src/Assembler.java
+++ src/Assembler.java
@@ -4,24 +4,29 @@ import java.io.IOException;
 public class Assembler {
     public static void main(String[] args) {
         try{
+            if(args.length == 0){
+                String[] temp = {"TestImmediate.asm"};
+                args = temp;
+            }
             Validate v = new Validate();
             boolean validated = v.validate(args);
             Options options = new Options(args);
-            if(v.validate(args) && options.getHelp()){
-                System.out.println();
+            if(validated && options.getHelp()){
+                System.out.println("this is the help");
             }
-            if(v.validate(args) && options.getBanner()){
-                System.out.println();
+            if(validated && options.getBanner()){
+                System.out.println("this is the banner");
             }
-            if(true && !options.getHelp() && !options.getBanner()){
+            if(validated && !options.getHelp() && !options.getBanner()){
                 SymbolTable symbolTable = new SymbolTable();
                 ErrorReporter reporter = new ErrorReporter();
-                InterRep IR = new Parser(new Scanner("TestImmediate.asm", reporter, symbolTable)).generates();
+                InterRep IR = new Parser(new Scanner(args[args.length - 1], reporter, symbolTable)).generates();
                 System.out.println(IR);
-                new CodeGenerator(IR, symbolTable, new File("TestImmediate.asm")).generateListing();
+                new CodeGenerator(IR, symbolTable, new File(args[args.length - 1])).generateListing();
                 reporter.report();
             }
         }catch(Exception e){
+            System.out.println(e.getMessage());
         }
 
     }
--- src/Validate.java
+++ src/Validate.java
@@ -1,14 +1,17 @@
+import java.io.File;
 import java.util.ArrayList;
 
 public class Validate implements IValidate {
 
     public boolean validate(String[] arg) {
-
+        System.out.println("validating");
+        // args from array to ArrayList
         ArrayList<String> args = new ArrayList<>();
         for(int i = 0; i < arg.length; i++){
             args.add(arg[i]);
         }
-
+        System.out.println(args);
+        //check for valide input (option or asm file)
         for (String ar: args){
             if(ar.equals("-h") || ar.equals("-help") || ar.equals("-v") || ar.equals("-verbose")
                     || ar.equals("-b") || ar.equals("-banner") || ar.equals("-l") || ar.equals("-listing") || ar.contains(".asm")){
@@ -18,7 +21,7 @@ public class Validate implements IValidate {
                 return false;
             }
         }
-
+        // ctr for options
         int optionsCtr = 0;
 
         if(args.contains("-h") || args.contains("-help")){
@@ -32,8 +35,6 @@ public class Validate implements IValidate {
                 return false;
             }
         }
-
-
         if(args.contains("-v") || args.contains("-verbose")){
             if(optionsCtr > 1){
                 System.out.println("Invalid input: more than one option is enabled");
@@ -65,9 +66,26 @@ public class Validate implements IValidate {
             optionsCtr++;
             if(!args.get(args.size() - 1).contains(".asm")){
                 System.out.println("-listing option requires an input file");
+                return false;
             }
 
         }
+
+        if(args.size() == 0){
+            System.out.println("no file or options passed to the program");
+            return false;
+        }
+
+        // check if .asm file exists
+        if(args.get(args.size() - 1).contains(".asm")){
+            File temp = new File(args.get(args.size() - 1));
+            if(!temp.exists()){
+                System.out.println("Input file does not exist");
+                return false;
+            }
+        }
+
+
         return true;
     }
 
